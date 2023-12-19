bf92a0441e9e71bcf89d7ca598cae56f1e6d142b
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-12-20 01:49:24 +0200

    add Flatpickr

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,4 +1,7 @@
+import Flatpickr from 'react-flatpickr';
+import { Russian } from "flatpickr/dist/l10n/ru.js"
 import css from "./Report.module.css";
+import "flatpickr/dist/flatpickr.css";
 
 const Report = () => {
   return (
@@ -8,6 +11,38 @@ const Report = () => {
           Формирование детализации отчетов WB в один файл
         </h1>
         <div className={css.forma}>
+        <div className={css.inputWrapper}>    
+        <label className={css.label}>Выберите период выборки:</label>  
+          <div className={css.dateWrapper}>
+          <Flatpickr
+                required
+                id='startDate'
+                name='startDate'
+                className={css.inputDate}
+                // onChange={date => setValue("startDate", date)}
+                // value={field.value}
+                options={{
+                  enableTime: false,
+                  locale: Russian,
+                  dateFormat: 'd.m.Y'
+                }}
+              />
+            <div>&#8212;</div>
+            <Flatpickr
+                required
+                id='endDate'
+                name='endDate'
+                className={css.inputDate}
+                // onChange={date => setValue("startDate", date)}
+                // value={field.value}
+                options={{
+                  enableTime: false,
+                  locale: Russian,
+                  dateFormat: 'd.m.Y'
+                }}
+              />
+          </div>
+          </div>  
           <div className={css.inputWrapper}>
             <label className={css.label}>Введите токен Статистики:</label>
             <input name="Token" className={css.inputToken} type="text" />
